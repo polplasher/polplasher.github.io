b90a40b1e7c8b4d93cdcb921b2d37c6e6860977a
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2026-01-08 20:49:51 -0300

    refactor

--- src/components/commons/navbar/notificationBell.jsx
+++ src/components/commons/navbar/notificationBell.jsx
@@ -11,7 +11,7 @@ export default function NotificationBell() {
     { id: 1, text: "Nuevo proyecto publicado 🚀", read: false },
     {
       id: 2,
-      text: "Hey como va?, te comento que actualmente estoy aprendiendo desarrollo móvil ✨",
+      text: "Hey como va?, te comento que actualmente estoy aprendiendo desarrollo móvil con React Native",
       read: false,
     },
   ];
